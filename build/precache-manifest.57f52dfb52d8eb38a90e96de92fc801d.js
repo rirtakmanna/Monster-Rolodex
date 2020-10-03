11e8b03c78be5c9ba6f31b99e9f947150344682c
rirtakmanna <web@rirtakmanna.com>
2020-10-03 18:17:11 +0530

    Update Build Folder

---
+++ build/precache-manifest.57f52dfb52d8eb38a90e96de92fc801d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9b64fde05f4fa6f56986a335d5f2d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c9eccb1bc0712c050e4",
+    "url": "/static/css/main.ab9f4ec2.chunk.css"
+  },
+  {
+    "revision": "03c3224f849a39b425f7",
+    "url": "/static/js/2.5f24d404.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f24d404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9eccb1bc0712c050e4",
+    "url": "/static/js/main.2fa514db.chunk.js"
+  },
+  {
+    "revision": "a55fe9c0b312e2774e66",
+    "url": "/static/js/runtime-main.80c5e1e7.js"
+  }
+]);
